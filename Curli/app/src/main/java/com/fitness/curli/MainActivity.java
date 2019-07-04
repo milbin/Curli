9cea0d0fdda39dd46d25795403302ebff2b86d67
Benjamin <Benjamintran0684@gmail.com>
2019-07-03 23:00:24 -0400

    added java files to android folder

---
+++ Curli/app/src/main/java/com/fitness/curli/MainActivity.java
@@ -0,0 +1,13 @@
+package com.fitness.curli;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
