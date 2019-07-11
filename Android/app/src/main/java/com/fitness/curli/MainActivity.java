53addee11ebd944331315c9b31c84f339bf96c32
Benjamin <Benjamintran0684@gmail.com>
2019-07-11 11:19:14 -0400

    added seperate activity for workoutView

--- Android/app/src/main/java/com/fitness/curli/MainActivity.java
+++ Android/app/src/main/java/com/fitness/curli/MainActivity.java
@@ -1,10 +1,12 @@
 package com.fitness.curli;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,21 +15,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        LinearLayout linearLayout = findViewById(R.id.linear_layout);
+        RelativeLayout workoutPlaceholder = findViewById(R.id.workout_card);
+        workoutPlaceholder.setOnClickListener(new onWorkoutClick());
 
-        View relativeLayout = LayoutInflater.from(this).inflate(R.layout.workout_card, null);
-        linearLayout.addView(relativeLayout);
-        TextView exerciseName = relativeLayout.findViewById(R.id.exercise_name);
-        exerciseName.setText("Leg Press");
 
-        View relativeLayout1 = LayoutInflater.from(this).inflate(R.layout.workout_card, null);
-        linearLayout.addView(relativeLayout1);
-        TextView exerciseName1 = relativeLayout1.findViewById(R.id.exercise_name);
-        exerciseName1.setText("Bicep Curl");
 
-        View relativeLayout2 = LayoutInflater.from(this).inflate(R.layout.workout_card, null);
-        linearLayout.addView(relativeLayout2);
-        TextView exerciseName2 = relativeLayout2.findViewById(R.id.exercise_name);
-        exerciseName2.setText("Tricep Curl");
+    }
+
+
+    public class onWorkoutClick implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            Intent myIntent = new Intent(MainActivity.this, WorkoutActivity.class);
+            startActivity(myIntent);
+        }
     }
 }
