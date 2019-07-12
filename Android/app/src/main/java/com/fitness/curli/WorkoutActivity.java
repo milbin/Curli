d031e1c6d03216732ca2b1f6bb8ea93d8c64dc6f
Benjamin <Benjamintran0684@gmail.com>
2019-07-12 14:15:45 -0400

    removed placeholder code

--- Android/app/src/main/java/com/fitness/curli/WorkoutActivity.java
+++ Android/app/src/main/java/com/fitness/curli/WorkoutActivity.java
@@ -135,45 +135,6 @@ public class WorkoutActivity extends AppCompatActivity {
                 TextView exerciseWeight = relativeLayout.findViewById(R.id.exercise_weight);
                 exerciseWeight.setText(Integer.toString((Integer) ((HashMap) exercise.get(setNumber + 2)).get("weight")));
             }
-
-
-
-
-
-
         }
-    }
-
-
-    public static boolean areDrawablesIdentical(Drawable drawableA, Drawable drawableB) {
-        Drawable.ConstantState stateA = drawableA.getConstantState();
-        Drawable.ConstantState stateB = drawableB.getConstantState();
-        // If the constant state is identical, they are using the same drawable resource.
-        // However, the opposite is not necessarily true.
-        return (stateA != null && stateB != null && stateA.equals(stateB))
-                || getBitmap(drawableA).sameAs(getBitmap(drawableB));
-    }
-
-    public static Bitmap getBitmap(Drawable drawable) {
-        Bitmap result;
-        if (drawable instanceof BitmapDrawable) {
-            result = ((BitmapDrawable) drawable).getBitmap();
-        } else {
-            int width = drawable.getIntrinsicWidth();
-            int height = drawable.getIntrinsicHeight();
-            // Some drawables have no intrinsic width - e.g. solid colours.
-            if (width <= 0) {
-                width = 1;
-            }
-            if (height <= 0) {
-                height = 1;
-            }
-
-            result = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
-            Canvas canvas = new Canvas(result);
-            drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
-            drawable.draw(canvas);
-        }
-        return result;
     }
 }
