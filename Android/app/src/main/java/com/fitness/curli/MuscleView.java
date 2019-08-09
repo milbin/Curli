bb11fafe15e8c4fe3ce9132a8b300eee0e42ff06
Theo Vasile <theo.vasile50@yahoo.com>
2019-08-09 16:54:43 -0400

    added muscleview class

---
+++ Android/app/src/main/java/com/fitness/curli/MuscleView.java
@@ -0,0 +1,12 @@
+package com.fitness.curli;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MuscleView extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.muscle_view);
+    }
+}
