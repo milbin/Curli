73596e9939790afdb1a266e6612945be45199404
Theo Vasile <theo.vasile50@yahoo.com>
2019-08-09 18:16:47 -0400

    added titled cards to the layout

--- Android/app/src/main/java/com/fitness/curli/MuscleView.java
+++ Android/app/src/main/java/com/fitness/curli/MuscleView.java
@@ -5,6 +5,10 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -36,8 +40,31 @@ public class MuscleView extends AppCompatActivity {
     }
 
     public void displayMuscles(){
-        ArrayList groups = SQLData.getGroups();
-        System.out.println(groups);
+        ArrayList<String> groups = SQLData.getGroups();
+
+        int groupNumber = groups.size();
+        int groupNameIndex = 0;
+        int cardsPerRow = 2;
+        boolean adding = true;
+
+        LinearLayout linearLayout = findViewById(R.id.MuscleViewLinearLayout);
+        while (adding){
+            LinearLayout rowLayout = new LinearLayout(this);
+            for (int x = 0; x < cardsPerRow; x++){
+                View card = LayoutInflater.from(context).inflate(R.layout.exercise_card_info_view, null);
+                TextView title = card.findViewById(R.id.title);
+                title.setText(groups.get(groupNameIndex));
+
+                rowLayout.addView(card);
+
+                if (groupNameIndex == groupNumber-1){
+                    adding = false;
+                    break;
+                }
+                groupNameIndex++;
+            }
+            linearLayout.addView(rowLayout);
+        }
 
         dialog.dismiss();
 
