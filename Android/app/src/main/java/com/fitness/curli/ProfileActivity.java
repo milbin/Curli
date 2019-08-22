61fea59f488a13243ab50653dde7acd8c7953ee5
JackEshkenazi <jack.eshkenazi@gmail.com>
2019-08-22 00:32:42 -0400

    spinner and variables for inputs

--- Android/app/src/main/java/com/fitness/curli/ProfileActivity.java
+++ Android/app/src/main/java/com/fitness/curli/ProfileActivity.java
@@ -18,6 +18,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
@@ -28,19 +29,41 @@ import android.widget.RelativeLayout;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.EditText;
+import android.widget.Toast;
 import android.widget.ToggleButton;
+import android.widget.Spinner;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.google.gson.Gson;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class ProfileActivity extends AppCompatActivity {
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
+
+public class ProfileActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
     RelativeLayout relativeLayout;
     ProgressDialog dialog;
     Context context;
     Menu menu;
+
+    //initialize variables
+    String userName;
+    String userEmail;
+    String currentWeight;
+    String weightGoal;
+    Boolean isNotifications;
+    String unitChoice;
+
+    //set weighing options
+    String[] weights={"Pounds","Kilograms"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,7 +71,6 @@ public class ProfileActivity extends AppCompatActivity {
         setContentView(R.layout.profile);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_profile);
-        toolbar.setTitle("Profile/Settings");
         toolbar.setTitleTextColor(Color.WHITE);
         setSupportActionBar(toolbar);
 
@@ -56,29 +78,56 @@ public class ProfileActivity extends AppCompatActivity {
 
         //retrieve values from edit texts into java variables
         EditText et = (EditText) findViewById(R.id.name_input);
-        String userName= et.getEditableText().toString();
+        userName= et.getEditableText().toString();
 
         et = (EditText) findViewById(R.id.email_input);
-        String userEmail = et.getEditableText().toString();
+        userEmail = et.getEditableText().toString();
 
         et = (EditText) findViewById(R.id.current_weight_input);
-        String currentWeight = et.getEditableText().toString();
+        currentWeight = et.getEditableText().toString();
 
         et = (EditText) findViewById(R.id.desired_weight_input);
-        String weightGoal = et.getEditableText().toString();
+        weightGoal = et.getEditableText().toString();
 
         //initiate the switch
         Switch notification_switch = (Switch) findViewById(R.id.notification_switch);
 
         // check current state of a Switch (true or false)
-        Boolean isNotifications = notification_switch.isChecked();
+        isNotifications = notification_switch.isChecked();
 
+        //Getting the instance of Spinner and applying OnItemSelectedListener on it
+        Spinner spin = (Spinner) findViewById(R.id.simpleSpinner);
+        spin.setOnItemSelectedListener(this);
 
+        //Creating the ArrayAdapter instance having the bank name list
+        ArrayAdapter aa = new ArrayAdapter(this,android.R.layout.simple_spinner_item,weights);
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-        System.out.println("IT WORKED FOOOO0LLL");
+        //Setting the ArrayAdapter data on the Spinner
+        spin.setAdapter(aa);
     }
 
 
+    //Performing action onItemSelected and onNothing selected
+    @Override
+    public void onItemSelected(AdapterView<?> arg0, View arg1, int position,long id) {
+
+        unitChoice = weights[position];
+
+        if(unitChoice.equals("Pounds")){
+            unitChoice = "lbs";
+        }
+        else if(unitChoice.equals("Kilograms")){
+            unitChoice = "kg";
+        }
+
+        Toast.makeText(getApplicationContext(), "Changed weight unit to " + weights[position], Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> arg0) {
+// TODO Auto-generated method stub
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -91,6 +140,10 @@ public class ProfileActivity extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
 
         }
+    }
+
+    public String getUnitChoice(){
+        return unitChoice;
     }
 
 }
