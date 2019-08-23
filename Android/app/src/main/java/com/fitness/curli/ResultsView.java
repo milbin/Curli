dbc3afe0f1a67c97bdeadda3cfc013d2b5740b45
Theo Vasile <theo.vasile50@yahoo.com>
2019-08-22 23:31:38 -0400

    added cards layout for each result

--- Android/app/src/main/java/com/fitness/curli/ResultsView.java
+++ Android/app/src/main/java/com/fitness/curli/ResultsView.java
@@ -1,12 +1,29 @@
 package com.fitness.curli;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class ResultsView extends AppCompatActivity {
+    ProgressDialog dialog;
+    Context context;
+
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.results_view);
+
+        dialog = ProgressDialog.show(this, "Loading...", "", false);
+
+        context = getApplicationContext();
+
+        displayResults();
+
+        dialog.dismiss();
+    }
+
+    public void displayResults(){
+
     }
 }
