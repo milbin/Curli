6429b4b0c30a97a683254a47138cd16145e7c538
Theo Vasile <theo.vasile50@yahoo.com>
2019-08-23 02:30:44 -0400

    cards added for each result

--- Android/app/src/main/java/com/fitness/curli/ResultsView.java
+++ Android/app/src/main/java/com/fitness/curli/ResultsView.java
@@ -4,6 +4,11 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class ResultsView extends AppCompatActivity {
     ProgressDialog dialog;
@@ -24,6 +29,14 @@ public class ResultsView extends AppCompatActivity {
     }
 
     public void displayResults(){
+        String[] resultsList = new String[]{"Weight", "Blood pressure", "Muscle Mass"};
+        LinearLayout resultLinearLayout = findViewById(R.id.resultsList);
 
+        for (String result : resultsList){
+            View card = LayoutInflater.from(context).inflate(R.layout.result_card, null);
+            TextView title = card.findViewById(R.id.title);
+            title.setText(result);
+            resultLinearLayout.addView(card);
+        }
     }
 }
