f39d5e2104c6085296e045c62d4cc94abd359c82
Benjamin <Benjamintran0684@gmail.com>
2019-08-30 16:13:27 -0400

    added new activity: schedulePlanner

---
+++ Android/app/src/main/java/com/fitness/curli/SchedulePlanner.java
@@ -0,0 +1,16 @@
+package com.fitness.curli;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SchedulePlanner extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+    }
+}
